3eee0ef5e4f0f9cf57e2a1fc178b03cee0a5f21f
urmilshroff <urmilshroff@gmail.com>
2019-06-17 00:59:29 +0530

    Experimenting with a gradient

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,66 +15,85 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> templateNames = [
+    'Material Design',
+    'Gradients',
+    'UI/UX',
+    'Android',
+    'iOS',
+    'GitHub'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:
           isThemeCurrentlyDark(context) ? MyColors.black : MyColors.white,
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(
-                left: 20.0, top: 50.0, bottom: 20.0, right: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'Dashboard Reborn',
-                  style: MyTextStyles.titleStyle,
-                ),
-                IconButton(
-                  icon: isThemeCurrentlyDark(context)
-                      ? Icon(Icons.brightness_5) //use sun icon
-                      : Icon(Icons.brightness_2), //use moon icon
-                  tooltip: isThemeCurrentlyDark(context)
-                      ? 'Switch to light mode'
-                      : 'Switch to dark mode',
-                  onPressed: () {
-                    DynamicTheme.of(context).setBrightness(
-                        Theme.of(context).brightness == Brightness.dark
-                            ? Brightness.light
-                            : Brightness.dark);
-                  },
-                ),
-              ],
+      body: Container(
+//        decoration: BoxDecoration(
+//            gradient: LinearGradient(
+//                colors: [
+//              MyColors.purple,
+//              MyColors.blue,
+//            ],
+//                begin: Alignment.topCenter,
+//                end: Alignment.bottomCenter,
+//                tileMode: TileMode.clamp)),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 20.0, top: 50.0, bottom: 20.0, right: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Dashboard Reborn',
+                    style: MyTextStyles.titleStyle,
+                  ),
+                  IconButton(
+                    icon: isThemeCurrentlyDark(context)
+                        ? Icon(Icons.brightness_5) //use sun icon
+                        : Icon(Icons.brightness_2), //use moon icon
+                    tooltip: isThemeCurrentlyDark(context)
+                        ? 'Switch to light mode'
+                        : 'Switch to dark mode',
+                    onPressed: () {
+                      DynamicTheme.of(context).setBrightness(
+                          Theme.of(context).brightness == Brightness.dark
+                              ? Brightness.light
+                              : Brightness.dark);
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-              child: StaggeredGridView.countBuilder(
-            crossAxisCount: 4,
-            itemCount: 8,
-            itemBuilder: (BuildContext context, int index) => buildTile(
-                  context,
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Card ${index + 1}',
-                          style: MyTextStyles.headingTheme,
-                          softWrap: false,
-                          overflow: TextOverflow.fade,
-                          maxLines: 1,
-                        )
-                      ]),
-                  onTap: null,
-                ),
-            staggeredTileBuilder: (int index) =>
-                StaggeredTile.count(2, index.isEven ? 2 : 1),
-            mainAxisSpacing: 10.0,
-            crossAxisSpacing: 10.0,
-          )),
-        ],
+            Expanded(
+                child: StaggeredGridView.countBuilder(
+              crossAxisCount: 4,
+              itemCount: templateNames.length,
+              itemBuilder: (BuildContext context, int index) => buildTile(
+                    context,
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '${templateNames[index]}',
+                            style: MyTextStyles.headingTheme,
+                            softWrap: false,
+                            overflow: TextOverflow.fade,
+                            maxLines: 1,
+                          )
+                        ]),
+                    onTap: null,
+                  ),
+              staggeredTileBuilder: (int index) =>
+                  StaggeredTile.count(2, index.isEven ? 2 : 1),
+              mainAxisSpacing: 5.0,
+              crossAxisSpacing: 5.0,
+            )),
+          ],
+        ),
       ),
     );
   }
