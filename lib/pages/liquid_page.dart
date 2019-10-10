063915000689394c0da386dba6d5a510aeb49293
urmilshroff <urmilshroff@gmail.com>
2019-10-10 16:40:58 +0530

    Completed Liquid Page

--- lib/pages/liquid_page.dart
+++ lib/pages/liquid_page.dart
@@ -1,4 +1,5 @@
 import 'package:dashboard_reborn/utils/colors.dart';
+import 'package:dashboard_reborn/utils/functions.dart';
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/Constants/Helpers.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
@@ -13,22 +14,6 @@ class _MyLiquidPageState extends State<MyLiquidPage> {
   Widget build(BuildContext context) {
     final pages = [
       Container(
-        color: MaterialColors.teal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text('Hello 1'),
-              ],
-            )
-          ],
-        ),
-      ),
-      Container(
         color: MaterialColors.pink,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -38,7 +23,29 @@ class _MyLiquidPageState extends State<MyLiquidPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text('Hello 2'),
+                Container(
+                  width: 300,
+                  height: 300,
+                  child: Container(
+                    width: 200.0,
+                    height: 200.0,
+                    child: Image.asset(
+                      'assets/images/icon/icon-android.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  '<< Swipe in any direction >>',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20.0,
+                    color: invertColorsMild(context),
+                  ),
+                ),
               ],
             )
           ],
@@ -54,7 +61,67 @@ class _MyLiquidPageState extends State<MyLiquidPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text('Hello 3'),
+                Container(
+                  width: 300,
+                  height: 300,
+                  child: Container(
+                    width: 200.0,
+                    height: 200.0,
+                    child: Image.asset(
+                      'assets/images/icon/icon-android.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  '<< Swipe in any direction >>',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20.0,
+                    color: invertColorsMild(context),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      Container(
+        color: MaterialColors.green,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 300,
+                  height: 300,
+                  child: Container(
+                    width: 200.0,
+                    height: 200.0,
+                    child: Image.asset(
+                      'assets/images/icon/icon-android.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  '<< Swipe in any direction >>',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20.0,
+                    color: invertColorsMild(context),
+                  ),
+                ),
               ],
             )
           ],
@@ -64,8 +131,9 @@ class _MyLiquidPageState extends State<MyLiquidPage> {
 
     return LiquidSwipe(
       pages: pages,
-      fullTransitionValue: 300.0,
-      enableLoop: false,
+      initialPage: 1,
+      fullTransitionValue: 350.0,
+      enableLoop: true,
       waveType: WaveType.liquidReveal,
     );
   }
